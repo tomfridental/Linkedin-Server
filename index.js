aa53346e55a149aaf4c176f2ccff24ba2226813d
tomfridental <tomfridental@gmail.com>
2018-12-29 18:11:40 +0200

    remove files

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.use(express.static(path.resolve('./build')))
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 
+app.get('/', (req,res) => {
+    res.send('Everythign is ok!')
+})
+
+
 app.get('*', (req, res)=> {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
